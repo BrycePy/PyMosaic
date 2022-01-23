b2e82f2e7fbf7e95cedeba4bfd8a925656a8a854
BrycePy <su287@bath.ac.uk>
2022-01-23 09:13:19 +0000

    add settings/ auto tile regeneration

--- calculate_tile.py
+++ calculate_tile.py
@@ -4,12 +4,7 @@ import os
 import json
 from multiprocessing import Pool, cpu_count
 import tqdm
-
-TWIDTH = 120  # this is width and height of a tile
-
-base_path = "tiles"
-temp_path = "tiles_temp"
-
+import settings
 
 def get_average_color(pipe_in):
     i, image_path = pipe_in
@@ -22,27 +17,36 @@ def get_average_color(pipe_in):
         half = (tile.width - tile.height) // 2
         tile = tile.crop((half, 0, half+tile.height, tile.height))
 
-    tile = tile.resize((TWIDTH, TWIDTH))
+    tile = tile.resize((settings.TILE_WIDTH, settings.TILE_WIDTH))
     tile = tile.convert("RGB")
-    target_path = os.path.join(temp_path, f"tile_{i}.png")
-    tile.save(target_path, format="png")
+    out_name = f"tile_{i}.jpeg"
+    temp_dir = settings.TILES_DIRECTORY+"_temp"
+    tile.save(os.path.join(temp_dir, out_name))
     r, g, b = tile.resize((1, 1)).getpixel((0, 0))
-    return [target_path, r, g, b]
+    return [out_name, r, g, b]
 
-
-if __name__ == "__main__":
-    directory_list = os.listdir(base_path)
-    file_list = [os.path.join(base_path, file_name)
+def main():
+    directory_list = os.listdir(settings.TILES_DIRECTORY)
+    file_list = [os.path.join(settings.TILES_DIRECTORY, file_name)
                  for file_name in directory_list]
     file_list = list(filter(lambda path: os.path.isfile(path), file_list))
+
+    temp_dir = settings.TILES_DIRECTORY+"_temp"
+    if not os.path.isdir(temp_dir):
+        os.makedirs(temp_dir)
 
     pool = Pool(cpu_count())
     results = tqdm.tqdm(pool.imap(get_average_color,
                         enumerate(file_list)), total=len(file_list))
 
-    color_list = dict(w=TWIDTH, tiles={})
+    color_list = dict(w=settings.TILE_WIDTH, tiles={})
     color_list["tiles"] = {x[0]: x[1:] for x in results}
 
-    tiles_data_path = os.path.join(temp_path, "tiles_data.json")
+    tiles_data_path = os.path.join(temp_dir, "tiles_data.json")
     with open(tiles_data_path, "w") as f:
         json.dump(color_list, f)
+
+    return color_list
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,62 +1,112 @@
 from PIL import Image
 import json
 import random
+from multiprocessing import Pool, cpu_count
+import tqdm
+import settings
+import os
+import calculate_tile
 
-
-with open("tiles_temp/tiles_data.json", "r") as f:
-    tile_data = json.load(f)
+def generate_row(pipe_in):
+    image_raw, tile_data, tile_directory = pipe_in
     tile_colors = tile_data.get("tiles")
+    TWIDTH = tile_data.get("w")
 
-TWIDTH = tile_data.get("w")
+    output_w = TWIDTH * image_raw.width
+    output_h = TWIDTH
 
-image_raw = Image.open("142072949-288-k241713.jpg")
+    pixel_reference = image_raw.convert("RGB")
+    frame_buffer = Image.new("RGB", (output_w, output_h))
 
-n = 400
-if image_raw.width > image_raw.height:
-    lr_w, lr_h = n, int(n*(image_raw.height/image_raw.width))
-else:
-    lr_w, lr_h = int(n*(image_raw.width/image_raw.height)), n
+    used = set()
 
-output_w = lr_w * TWIDTH
-output_h = lr_h * TWIDTH
+    def calculate_error(color1, color2):
+        if not color1: return 999999999
+        r_diff = abs(color1[0] - color2[0])
+        g_diff = abs(color1[1] - color2[1])
+        b_diff = abs(color1[2] - color2[2])
+        w_diff = (r_diff + g_diff + b_diff)
+        error = r_diff**2 + g_diff**2 + b_diff**2 + w_diff**2
+        return error
 
-pixel_reference = image_raw.resize((lr_w, lr_h)).convert("RGB")
-frame_buffer = Image.new("RGB", (output_w, output_h))
+    def get_nearest_color(target_color):
+        if (settings.AVOID_ROW_DUPE): 
+            colors = [(calculate_error(tile_colors[tile], target_color), tile) for tile in tile_colors if tile not in used]
+        else:
+            colors = [(calculate_error(tile_colors[tile], target_color), tile) for tile in tile_colors]
+        best = min(colors)
+        boundary = max(settings.NOISE_LEVEL, best[0]*2)
+        colors = [x for x in colors if x[0] <= boundary]
+        tile = random.choice(colors)[1]
+        used.add(tile)
+        return tile
 
-def calculate_error(color1, color2):
-    if not color1: return 999999999
-    r_diff = abs(color1[0] - color2[0])
-    g_diff = abs(color1[1] - color2[1])
-    b_diff = abs(color1[2] - color2[2])
-    w_diff = (r_diff + g_diff + b_diff)
-    error = r_diff**2 + g_diff**2 + b_diff**2 + w_diff**2
-    return error
-
-last_color = None
-last_list = None
-def get_nearest_color(target_color):
-    global last_list, last_color
-    if calculate_error(last_color, target_color) < 1000:
-        r = (random.randint(0, random.randint(3, 10)))
-        return last_list[r][1]
-    test = [(calculate_error(tile_colors[tile], target_color), tile) for tile in tile_colors]
-    test = sorted(test)
-    last_list = test[:100]
-    last_color = target_color
-    r = (random.randint(0, random.randint(3, 10)))
-    return test[r][1]
-
-for x in range(pixel_reference.width):
-    for y in range(pixel_reference.height):
-        target_color = pixel_reference.getpixel((x, y))
-        tile_path = get_nearest_color(target_color)
-        paste_position = (x*TWIDTH, y*TWIDTH)
+    random_order = list(range(pixel_reference.width))
+    random.shuffle(random_order)
+    for x in random_order:
+        target_color = pixel_reference.getpixel((x, 0))
+        tile_name = get_nearest_color(target_color)
+        paste_position = (x*TWIDTH, 0)
+        tile_path = os.path.join(tile_directory, tile_name)
         tile = Image.open(tile_path)
         frame_buffer.paste(tile, paste_position)
-    print(x)
+    frame_buffer.resize((output_w, output_h))
+    return frame_buffer, used
 
-frame_buffer_2 = Image.new("RGBA", (output_w, output_h))
-mask = image_raw.convert("RGBA").split()[-1].resize((output_w, output_h))
-frame_buffer_2.paste(frame_buffer, (0,0), mask)
+def render(input_file, n, tile_directory, output_file="output.jpeg"):
+    color_data_path = os.path.join(tile_directory, "tiles_data.json")
+    if not(os.path.isdir(tile_directory)) or not(os.path.isfile(color_data_path)):
+        print("generating tiles...")
+        tile_data = calculate_tile.main()
+        tile_width = tile_data.get("w")
 
-frame_buffer_2.save("output.png")
+    with open(color_data_path, "r") as f:
+        tile_data = json.load(f)
+        tile_width = tile_data.get("w")
+
+    if tile_width != settings.TILE_WIDTH:
+        print("generating tiles...")
+        tile_data = calculate_tile.main()
+        tile_width = tile_data.get("w")
+
+    image_raw = Image.open(input_file)
+
+    if image_raw.width > image_raw.height:
+        lr_w, lr_h = n, int(n*(image_raw.height/image_raw.width))
+    else:
+        lr_w, lr_h = int(n*(image_raw.width/image_raw.height)), n
+
+    pixel_reference = image_raw.resize((lr_w, lr_h)).convert("RGB")
+
+
+    works = []
+    for i in range(pixel_reference.height-1):
+        strip = pixel_reference.crop((0, i, pixel_reference.width, i+1))
+        works.append((strip, tile_data, tile_directory))
+
+    print("rendering rows....")
+    pool = Pool(cpu_count())
+    strips = tqdm.tqdm(pool.imap(generate_row, works), total=len(works))
+    strips = list(strips)
+    output_w = lr_w * tile_width
+    output_h = lr_h * tile_width
+    frame_buffer = Image.new("RGB", (output_w, output_h))
+
+    print("combining rows....")
+    used_tiles = []
+    for i, result in tqdm.tqdm(enumerate(strips), total=len(strips)):
+        strip, used = result
+        frame_buffer.paste(strip, (int((i%2/2-0.25)*tile_width), i*tile_width))
+        used_tiles.extend(list(used))
+    
+    used_tiles = set(used_tiles)
+    print(len(used_tiles), "unique tile used.")
+    print("saving to file....")
+
+    frame_buffer.save(output_file, optimize=settings.OUTPUT_OPTIMIZE)
+
+if __name__ == '__main__':
+    render( settings.INPUT_FILE,
+            settings.MAX_TILES, 
+            settings.TILES_DIRECTORY+"_temp", 
+            settings.OUTPUT_FILE)
